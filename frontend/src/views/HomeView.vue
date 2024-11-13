a65b999602cbed08fe61d7c31a674d99f10f15fc
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-11-14 00:28:43 +0100

    feat: update HomeView layout and welcome message, add adjust tests

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
-        <h1>Welcome to the Vehicle Cost Calculator</h1>
-        <router-link to="/calculate-vehicle-cost" style="text-decoration: none; color: inherit">
-            Go to Calculate Vehicle Cost
-        </router-link>
-    </div>
+    <v-container>
+        <h1 class="text-h2 text-center my-5">Welcome to Progi services</h1>
+        <v-row justify="center" class="my-10 py-10">
+            <v-col cols="12" md="8">
+                <router-link to="/calculate-vehicle-cost" class="text-decoration-none">
+                    <v-card class="pa-5" outlined>
+                        <v-card-text class="text-center text-h4">
+                            Vehicle cost simulator Service
+                        </v-card-text>
+                    </v-card>
+                </router-link>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
