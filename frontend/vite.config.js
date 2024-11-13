8f2416aa7dde16243cce39c5c715edbf3c6a8395
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2024-11-13 14:02:57 +0100

    Setup Unit tests enviroment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
@@ -6,5 +7,19 @@ export default defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
     optimizeDeps: {
         include: ["vuetify"], // Ensures Vuetify is pre-bundled
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        server: {
+            deps: {
+                inline: ["vuetify"],
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
 });
